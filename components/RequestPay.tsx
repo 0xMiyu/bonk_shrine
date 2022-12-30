0d7451ebf8db8e1c3c66dda15c309d8f7a9fd17f
dylan <thedylone@gmail.com>
2022-12-30 20:55:29 +0000

    change

--- components/RequestPay.tsx
+++ components/RequestPay.tsx
@@ -22,7 +22,7 @@ export const RequestPay: FC = () => {
     const BONK_COST = 1;
     const BONK_DECIMALS = 5;
     const RECEIVING_BONK_ATA = "5rmWy289CSN1XbHeRRQLwiv6ed3k64ipgKCzdpoFAX3E";
-    const [components, setComponents] = useState([""]); 
+    const [components, setComponents] = useState([""]);
     const wrapperRef = useRef(null);
     useOutsideAlerter(wrapperRef);
     const onClick = useCallback(async () => {
@@ -39,7 +39,7 @@ export const RequestPay: FC = () => {
             //     new PublicKey(BONK_TOKEN_ADDRESS),
             //     publicKey
             // )
-            
+
             // const ix = createTransferCheckedInstruction(
             //     source_account,
             //     new PublicKey(BONK_TOKEN_ADDRESS),
@@ -61,12 +61,12 @@ export const RequestPay: FC = () => {
             //     signature: tx,
             // });
             alert("Transaction Confirmed!");
-            
-  
-            setComponents([...components, "Sample Component"]) 
-   
+
+
+            setComponents([...components, "Sample Component"])
+
             console.log("fuck")
-    
+
 
         } catch (error: any) {
             alert(error);
@@ -76,19 +76,37 @@ export const RequestPay: FC = () => {
 
     return (
         // <div ref={wrapperRef}>
-        <div>
-            {components.map((item, i) => (<img className={styles.coin} src="BonkLogo.webp"></img> ))}
-            <br/>
-            <br/>
-            <br/>
-            <button
-                className="px-8 m-2 btn animate-pulse bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ..."
-                onClick={onClick}
-            >
-                <span>PAY BONK!</span>
-            </button>
-            
+        <div className={styles.shrine}>
+                <svg width="640" height="480" preserveAspectRatio="xMidYMin" viewBox="0 0 640 480">
+                    <rect id="svg_29" height="12" width="486" y="66.86667" x="84.53333" stroke-linecap="null"
+                        stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="#3f3f3f" />
+                    <rect id="svg_30" height="12" width="470" y="82.86667" x="90.53333" stroke-linecap="null"
+                        stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="#ff0000" />
+                    <rect id="svg_31" height="18" width="471" y="127.86667" x="89.53333" stroke-linecap="null"
+                        stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="#ff0000" />
+                    <rect id="svg_32" height="297" width="25" y="94.86667" x="153.53333" stroke-linecap="null"
+                        stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="#ff0000" />
+                    <rect id="svg_33" height="303" width="28" y="94.86667" x="476.53333" stroke-linecap="null"
+                        stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="#ff0000" />
+                    <rect id="svg_34" height="31" width="21" y="95.86667" x="323.53333" stroke-linecap="null"
+                        stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="#ff0000" />
+                    <rect id="svg_35" height="33" width="38" y="389.86667" x="146.53333" stroke-linecap="null"
+                        stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="#000000" />
+                    <rect id="svg_37" height="33" width="38" y="395.2" x="471.2" stroke-linecap="null"
+                        stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="#000000" />
+                </svg>
+                <div className={styles.shrine__overlay}>
+                    <img src="overlay.gif"></img>
+                </div>
+                {components.map((item, i) => (<img key={i} className={styles.coin} src="BonkLogo.webp"></img>))}
+                <div className={styles.pay__button__wrapper}>
 
-        </div>
-    );
+                    <button className={styles.pay__button} onClick={onClick}>
+                        <span>PAY BONK!</span>
+                    </button>
+
+                </div>
+
+            </div>
+            );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,35 +18,16 @@ export default function Home() {
         </Head>
 
         <main className={styles.main}>
+          <div className={styles.wallet_button_container}>
+            <WalletMultiButton className={styles.wallet_button + " btn btn-ghost mr-4"} />
+          </div>
+
           <h1 className={styles.title}>
             Welcome to the Shrine of Bonk!
           </h1>
           <br />
 
-          <div className={styles.shrine}>
-            <svg width="640" height="480" preserveAspectRatio="xMidYMin" viewBox="0 0 640 480">
-              <rect id="svg_29" height="12" width="486" y="66.86667" x="84.53333" stroke-linecap="null"
-                stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="#3f3f3f" />
-              <rect id="svg_30" height="12" width="470" y="82.86667" x="90.53333" stroke-linecap="null"
-                stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="#ff0000" />
-              <rect id="svg_31" height="18" width="471" y="127.86667" x="89.53333" stroke-linecap="null"
-                stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="#ff0000" />
-              <rect id="svg_32" height="297" width="25" y="94.86667" x="153.53333" stroke-linecap="null"
-                stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="#ff0000" />
-              <rect id="svg_33" height="303" width="28" y="94.86667" x="476.53333" stroke-linecap="null"
-                stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="#ff0000" />
-              <rect id="svg_34" height="31" width="21" y="95.86667" x="323.53333" stroke-linecap="null"
-                stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="#ff0000" />
-              <rect id="svg_35" height="33" width="38" y="389.86667" x="146.53333" stroke-linecap="null"
-                stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="#000000" />
-              <rect id="svg_37" height="33" width="38" y="395.2" x="471.2" stroke-linecap="null"
-                stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="#000000" />
-            </svg>
-            <div className={styles.shrine__overlay}>
-              <img src="overlay.gif"></img>
-            </div>
-            <img className={styles.coin} src="BonkLogo.webp"></img>
-          </div>
+          <RequestPay />
 
           {/* <div className={styles.fortune_container}>
             <div className={styles.fortune}>
@@ -55,14 +36,6 @@ export default function Home() {
               <div>col 3</div>
             </div>
           </div> */}
-
-
-          <br />
-          <WalletMultiButton className="btn btn-ghost mr-4" />
-          <br />
-          <br />
-
-          <RequestPay />
 
 
         </main>
