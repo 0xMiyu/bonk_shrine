0768939955ae032f8079831f869bd40ca3ab91bc
AstraI <ong.martin24@gmail.com>
2022-12-31 00:27:08 +0800

    first commit

---
+++ components/RequestPay.tsx
@@ -0,0 +1,73 @@
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import {
+    Transaction,
+    LAMPORTS_PER_SOL,
+    TransactionSignature,
+    SystemProgram,
+    PublicKey,
+} from "@solana/web3.js";
+import { FC, useCallback } from "react";
+import { getAssociatedTokenAddress, createTransferCheckedInstruction } from "@solana/spl-token";
+
+
+export const RequestPay: FC = () => {
+    const { connection } = useConnection();
+    const { publicKey, sendTransaction } = useWallet();
+
+    const BONK_TOKEN_ADDRESS = "DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263";
+    const BONK_DECIMALS = 5;
+    const RECEIVING_BONK_ATA = "5rmWy289CSN1XbHeRRQLwiv6ed3k64ipgKCzdpoFAX3E";
+
+    const onClick = useCallback(async () => {
+        if (!publicKey) {
+            console.log("error", "Wallet not connected!");
+            alert("Wallet not Connected!");
+            return;
+        }
+
+        const transaction = new Transaction();
+        try {
+
+            const source_account = await getAssociatedTokenAddress(
+                new PublicKey(BONK_TOKEN_ADDRESS),
+                publicKey
+            )
+            
+            const ix = createTransferCheckedInstruction(
+                source_account,
+                new PublicKey(BONK_TOKEN_ADDRESS),
+                new PublicKey(RECEIVING_BONK_ATA),
+                publicKey,
+                10000,
+                BONK_DECIMALS
+            );
+            transaction.add(ix);
+
+            const tx = await sendTransaction(transaction, connection);
+            await connection.confirmTransaction({
+                blockhash: (
+                    await connection.getLatestBlockhash("max")
+                ).blockhash,
+                lastValidBlockHeight: (
+                    await connection.getLatestBlockhash("max")
+                ).lastValidBlockHeight,
+                signature: tx,
+            });
+            alert("Transaction Confirmed!");
+        } catch (error: any) {
+            alert(error);
+            console.log(error);
+        }
+    }, [publicKey, connection]);
+
+    return (
+        <div>
+            <button
+                className="px-8 m-2 btn animate-pulse bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ..."
+                onClick={onClick}
+            >
+                <span>PAY BONK!</span>
+            </button>
+        </div>
+    );
+};
---
+++ pages/index.tsx
@@ -0,0 +1,47 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { ContextProvider } from '../contexts/ContextProvider';
+import { RequestPay } from '../components/RequestPay';
+require('@solana/wallet-adapter-react-ui/styles.css');
+
+
+export default function Home() {
+  return (
+    <ContextProvider>
+    <div className={styles.container}>
+      <Head>
+        <title>Solana lfg</title>
+        <meta name="description" content="kekw" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to the Shrine of Bonk!
+        </h1>
+        <br />
+        <br />
+        <WalletMultiButton className="btn btn-ghost mr-4" />
+        <br />
+        <br />
+        
+        <RequestPay/>
+        
+        
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          kmskmskmskmskms
+        </a>
+      </footer>
+    </div>
+    </ContextProvider>
+  )
+}
