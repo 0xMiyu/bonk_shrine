c21dd95d5663baddee1e566f465193177324dcce
dylan <thedylone@gmail.com>
2022-12-31 12:09:57 +0000

    fix display part 4

--- components/RequestPay.tsx
+++ components/RequestPay.tsx
@@ -210,7 +210,7 @@ export const RequestPay: FC = () => {
             <div className={styles.pay__button__wrapper}>
 
                 <button className={styles.pay__button} onClick={onClick}>
-                    <span>PAY BONK!</span>
+                    <span>PAY BONK! 🙏</span>
                 </button>
 
             </div>
