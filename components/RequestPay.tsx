828c751f36f2912111ba20cd5f96774b4c072a4c
dylan <thedylone@gmail.com>
2022-12-31 00:12:55 +0000

    fortune slip

--- components/RequestPay.tsx
+++ components/RequestPay.tsx
@@ -78,22 +78,22 @@ export const RequestPay: FC = () => {
         // <div ref={wrapperRef}>
         <div className={styles.shrine}>
                 <svg width="640" height="480" preserveAspectRatio="xMidYMin" viewBox="0 0 640 480">
-                    <rect id="svg_29" height="12" width="486" y="66.86667" x="84.53333" stroke-linecap="null"
-                        stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="#3f3f3f" />
-                    <rect id="svg_30" height="12" width="470" y="82.86667" x="90.53333" stroke-linecap="null"
-                        stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="#ff0000" />
-                    <rect id="svg_31" height="18" width="471" y="127.86667" x="89.53333" stroke-linecap="null"
-                        stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="#ff0000" />
-                    <rect id="svg_32" height="297" width="25" y="94.86667" x="153.53333" stroke-linecap="null"
-                        stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="#ff0000" />
-                    <rect id="svg_33" height="303" width="28" y="94.86667" x="476.53333" stroke-linecap="null"
-                        stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="#ff0000" />
-                    <rect id="svg_34" height="31" width="21" y="95.86667" x="323.53333" stroke-linecap="null"
-                        stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="#ff0000" />
-                    <rect id="svg_35" height="33" width="38" y="389.86667" x="146.53333" stroke-linecap="null"
-                        stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="#000000" />
-                    <rect id="svg_37" height="33" width="38" y="395.2" x="471.2" stroke-linecap="null"
-                        stroke-linejoin="null" stroke-dasharray="null" stroke-width="5" stroke="#000000" fill="#000000" />
+                    <rect id="svg_29" height="12" width="486" y="66.86667" x="84.53333" 
+                          strokeWidth="5" stroke="#000000" fill="#3f3f3f" />
+                    <rect id="svg_30" height="12" width="470" y="82.86667" x="90.53333" 
+                          strokeWidth="5" stroke="#000000" fill="#ff0000" />
+                    <rect id="svg_31" height="18" width="471" y="127.86667" x="89.53333" 
+                          strokeWidth="5" stroke="#000000" fill="#ff0000" />
+                    <rect id="svg_32" height="297" width="25" y="94.86667" x="153.53333" 
+                          strokeWidth="5" stroke="#000000" fill="#ff0000" />
+                    <rect id="svg_33" height="303" width="28" y="94.86667" x="476.53333" 
+                          strokeWidth="5" stroke="#000000" fill="#ff0000" />
+                    <rect id="svg_34" height="31" width="21" y="95.86667" x="323.53333" 
+                          strokeWidth="5" stroke="#000000" fill="#ff0000" />
+                    <rect id="svg_35" height="33" width="38" y="389.86667" x="146.53333" 
+                          strokeWidth="5" stroke="#000000" fill="#000000" />
+                    <rect id="svg_37" height="33" width="38" y="395.2" x="471.2" 
+                          strokeWidth="5" stroke="#000000" fill="#000000" />
                 </svg>
                 <div className={styles.shrine__overlay}>
                     <img src="overlay.gif"></img>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import styles from '../styles/Home.module.css'
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { ContextProvider } from '../contexts/ContextProvider';
 import { RequestPay } from '../components/RequestPay';
+import { TwitterShareButton, TwitterIcon } from 'react-share';
 require('@solana/wallet-adapter-react-ui/styles.css');
 
 
@@ -29,13 +30,25 @@ export default function Home() {
 
           <RequestPay />
 
-          {/* <div className={styles.fortune_container}>
+          <div className={styles.fortune_container}>
             <div className={styles.fortune}>
-              <div>col 1</div>
-              <div>col 2</div>
-              <div>col 3</div>
+              <div className={styles.fortune_result}>大吉</div>
+
+              <div className={styles.fortune_desc}>description goes here Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+              <div className={styles.fortune_share}>
+                Share your fortune
+                <div className={styles.fortune_share_buttons}>
+                  <TwitterShareButton
+                    url={"insert url here"}
+                    title={"I got " + "大吉" + " from the Shrine of Bonk!"}
+                    className="Demo__some-network__share-button">
+                    <TwitterIcon size={32} round />
+                  </TwitterShareButton>
+                </div>
+                <button className={styles.fortune_share_retry} onClick={() => window.location.reload()}>Retry</button>
+              </div>
             </div>
-          </div> */}
+          </div>
 
 
         </main>
