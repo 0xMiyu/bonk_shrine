5450df0eb9911843decbc86c2ead0b15db64d407
dylan <thedylone@gmail.com>
2023-01-01 15:32:52 +0000

    try fix card part 4

---
+++ pages/_document.tsx
@@ -0,0 +1,34 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html lang="en">
+            <Head>
+                <meta charSet="utf-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <link rel="icon" href="/torii.svg" />
+
+                <meta name="description" content="Get your fortune read at Hatsumode" />
+
+                <meta key="og_url" property="og:url" content="https://www.bonkshrine.com/" />
+                <meta key="og_type" property="og:type" content="website" />
+                <meta key="og_title" property="og:title" content="Shrine of Bonk" />
+                <meta key="og_desc" property="og:description" content="Get your fortune read at Hatsumode" />
+                <meta key="og_img" property="og:image" content="https://www.bonkshrine.com/" />
+
+                <meta key="twitter_card" name="twitter:card" content="summary_large_image" />
+                <meta key="twitter_domain" property="twitter:domain" content="https://www.bonkshrine.com/" />
+                <meta key="twitter_url" property="twitter:url" content="https://www.bonkshrine.com/" />
+                <meta key="twitter_title" name="twitter:title" content="Shrine of Bonk" />
+                <meta key="twitter_desc" name="twitter:description" content="Get your fortune read at Hatsumode" />
+                <meta key="twitter_img" name="twitter:image" content="https://www.bonkshrine.com/card.png" />
+
+
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
